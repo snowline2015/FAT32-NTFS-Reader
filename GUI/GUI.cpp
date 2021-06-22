930d6e26a9f49741a09e3ec1eea3f1df36cc35dc
HuyHoang19127406 <63275057+HuyHoang19127406@users.noreply.github.com>
2021-06-22 16:44:10 +0700

    gui

---
+++ GUI/GUI.cpp
@@ -0,0 +1,12 @@
+#include "GUI.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+void Main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	GUI::GUI form;
+	Application::Run(% form);
+}
---
+++ GUI/GUI.h
@@ -0,0 +1,209 @@
+#pragma once
+
+namespace GUI {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for GUI
+	/// </summary>
+	public ref class GUI : public System::Windows::Forms::Form
+	{
+	public:
+		GUI(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~GUI()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::SplitContainer^ splitContainer1;
+	protected:
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TreeView^ treeView1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::ComboBox^ comboBox1;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::ListView^ listView1;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(GUI::typeid));
+			this->splitContainer1 = (gcnew System::Windows::Forms::SplitContainer());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->treeView1 = (gcnew System::Windows::Forms::TreeView());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->listView1 = (gcnew System::Windows::Forms::ListView());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->splitContainer1))->BeginInit();
+			this->splitContainer1->Panel1->SuspendLayout();
+			this->splitContainer1->Panel2->SuspendLayout();
+			this->splitContainer1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			this->splitContainer1->Dock = System::Windows::Forms::DockStyle::Fill;
+			this->splitContainer1->Location = System::Drawing::Point(0, 0);
+			this->splitContainer1->Name = L"splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this->splitContainer1->Panel1->BackColor = System::Drawing::Color::WhiteSmoke;
+			this->splitContainer1->Panel1->Controls->Add(this->label2);
+			this->splitContainer1->Panel1->Controls->Add(this->label1);
+			this->splitContainer1->Panel1->Controls->Add(this->treeView1);
+			this->splitContainer1->Panel1->Controls->Add(this->button1);
+			this->splitContainer1->Panel1->Controls->Add(this->comboBox1);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this->splitContainer1->Panel2->BackColor = System::Drawing::Color::White;
+			this->splitContainer1->Panel2->Controls->Add(this->listView1);
+			this->splitContainer1->Panel2->Controls->Add(this->label3);
+			this->splitContainer1->Size = System::Drawing::Size(782, 453);
+			this->splitContainer1->SplitterDistance = 379;
+			this->splitContainer1->TabIndex = 0;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Segoe UI Semibold", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(92, 418);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(13, 20);
+			this->label2->TabIndex = 4;
+			this->label2->Text = L" ";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Segoe UI", 10.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(26, 416);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(51, 23);
+			this->label1->TabIndex = 3;
+			this->label1->Text = L"Path:";
+			this->label1->Click += gcnew System::EventHandler(this, &GUI::label1_Click);
+			// 
+			// treeView1
+			// 
+			this->treeView1->BackColor = System::Drawing::Color::WhiteSmoke;
+			this->treeView1->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->treeView1->Location = System::Drawing::Point(30, 61);
+			this->treeView1->Name = L"treeView1";
+			this->treeView1->Size = System::Drawing::Size(331, 343);
+			this->treeView1->TabIndex = 2;
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::Gainsboro;
+			this->button1->FlatAppearance->BorderColor = System::Drawing::Color::Gainsboro;
+			this->button1->FlatAppearance->MouseDownBackColor = System::Drawing::Color::LightGray;
+			this->button1->FlatAppearance->MouseOverBackColor = System::Drawing::Color::LightGray;
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Segoe UI Semibold", 7.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->Location = System::Drawing::Point(275, 15);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(86, 28);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"Choose";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &GUI::button1_Click);
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->BackColor = System::Drawing::Color::WhiteSmoke;
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Location = System::Drawing::Point(30, 16);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(229, 24);
+			this->comboBox1->TabIndex = 0;
+			this->comboBox1->SelectedIndexChanged += gcnew System::EventHandler(this, &GUI::comboBox1_SelectedIndexChanged);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Segoe UI", 10.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(146, 22);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(106, 23);
+			this->label3->TabIndex = 0;
+			this->label3->Text = L"Information";
+			// 
+			// listView1
+			// 
+			this->listView1->BackColor = System::Drawing::Color::White;
+			this->listView1->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->listView1->Font = (gcnew System::Drawing::Font(L"Segoe UI Semibold", 9, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->listView1->HideSelection = false;
+			this->listView1->Location = System::Drawing::Point(42, 61);
+			this->listView1->Name = L"listView1";
+			this->listView1->Size = System::Drawing::Size(323, 343);
+			this->listView1->TabIndex = 1;
+			this->listView1->UseCompatibleStateImageBehavior = false;
+			// 
+			// GUI
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(782, 453);
+			this->Controls->Add(this->splitContainer1);
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+			this->Name = L"GUI";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"GUI";
+			this->splitContainer1->Panel1->ResumeLayout(false);
+			this->splitContainer1->Panel1->PerformLayout();
+			this->splitContainer1->Panel2->ResumeLayout(false);
+			this->splitContainer1->Panel2->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->splitContainer1))->EndInit();
+			this->splitContainer1->ResumeLayout(false);
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void comboBox1_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+};
+}
